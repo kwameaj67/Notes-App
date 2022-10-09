aebe66d94c5aa1bf5da0af79f7fb6689a27b7dde
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-09 14:53:05 +0000

    added fonts

---
+++ Notes-Clone/Constants/Font.swift
@@ -0,0 +1,16 @@
+//
+//  Font.swift
+//  Notes-Clone
+//
+//  Created by Kwame Agyenim - Boateng on 09/10/2022.
+//
+
+import Foundation
+
+enum Font: String {
+    case bold = "EuclidCircularB-Bold"
+    case semi_bold = "EuclidCircularB-SemiBold"
+    case medium = "EuclidCircularB-Medium"
+    case regular = "EuclidCircularB-Regular"
+    case light = "EuclidCircularB-Light"
+}
