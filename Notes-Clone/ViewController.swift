50eeaefdcc4531ccdab341e443569966ff2c1988
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-09 11:34:42 +0000

    Initial Commit

---
+++ Notes-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes-Clone
+//
+//  Created by Kwame Agyenim - Boateng on 09/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
