06c9f95d2663a6b73507eb572572ea3ac64b778f
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-09 14:52:52 +0000

    colors

---
+++ Notes-Clone/Constants/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Notes-Clone
+//
+//  Created by Kwame Agyenim - Boateng on 09/10/2022.
+//
+
+import UIKit
+
+
+struct Color {
+    static let red = UIColor(red: 0.80, green: 0.31, blue: 0.31, alpha: 1.00)
+    static let dark = UIColor(red: 0.10, green: 0.11, blue: 0.13, alpha: 1.00)
+    static let grey = UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 1.00)
+}
