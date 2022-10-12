bf5187e5463497e9c9c83d22fcbe9fc96684435b
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-12 15:38:53 +0000

    custom button

---
+++ Notes-Clone/Views/NButton.swift
@@ -0,0 +1,26 @@
+//
+//  NButton.swift
+//  Notes-Clone
+//
+//  Created by Kwame Agyenim - Boateng on 10/10/2022.
+//
+
+import UIKit
+
+class NButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = Color.dark
+        layer.cornerRadius = self.frame.height/2
+        layer.masksToBounds = false
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont(name: Font.semi_bold.rawValue, size: 16)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
