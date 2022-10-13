bde265c714453508d1c09554533a58e89730a6f5
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-13 21:59:51 +0000

    app config

--- Notes-Clone/SceneDelegate.swift
+++ Notes-Clone/SceneDelegate.swift
@@ -13,10 +13,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let userDefaults = UserDefaults.standard
+        window =  UIWindow(frame: UIScreen.main.bounds)
+        window?.windowScene = windowScene
+        
+        if userDefaults.bool(forKey: "isLoggedIn"){
+            window?.rootViewController = UINavigationController(rootViewController: FolderVC())
+        }else{
+            window?.rootViewController = UINavigationController(rootViewController: NameVC())
+        }
+       
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -45,6 +55,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+       
     }
 
 
