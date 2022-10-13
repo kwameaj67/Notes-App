9c62ab7d567c8e9a7b88753313e637da4d38024a
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-13 22:01:02 +0000

    notes vc's

---
+++ Notes-Clone/Controllers/Note/AddNoteVC.swift
@@ -0,0 +1,21 @@
+//
+//  AddNoteVC.swift
+//  Notes-Clone
+//
+//  Created by Kwame Agyenim - Boateng on 09/10/2022.
+//
+
+import UIKit
+
+class AddNoteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+   
+
+}
