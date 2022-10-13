3a9ce82ec74eb54eb84981d3bb01248d68b451dc
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-13 21:59:34 +0000

    colors

--- Notes-Clone/Constants/Color.swift
+++ Notes-Clone/Constants/Color.swift
@@ -12,4 +12,7 @@ struct Color {
     static let red = UIColor(red: 0.80, green: 0.31, blue: 0.31, alpha: 1.00)
     static let dark = UIColor(red: 0.10, green: 0.11, blue: 0.13, alpha: 1.00)
     static let grey = UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 1.00)
+    static let bg = UIColor(red: 0.99, green: 0.99, blue: 0.99, alpha: 1.00)
+    static let pad_bg = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.00)
+    static let cell_dark_bg = UIColor(red: 0.14, green: 0.15, blue: 0.17, alpha: 1.00)
 }
