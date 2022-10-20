97f41cd982742e362d65e5604e8c8168593d9d94
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-20 20:34:56 +0000

    compositional layout

--- Notes-Clone/Controllers/Note/NotesVC.swift
+++ Notes-Clone/Controllers/Note/NotesVC.swift
@@ -8,22 +8,112 @@
 import UIKit
 
 class NotesVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    let notesData = NoteType.data
+    var notes : Note!
+    var folderTitle: String? = "" {
+        didSet{
+            self.navigationController?.navigationBar.largeContentTitle = folderTitle
+            title = folderTitle
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Color.bg
+        setupViews()
+        setupContraints()
+       
     }
-    */
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureNavBar()
+    }
+    // MARK: Properties -
+    lazy var notesCollectionView: UICollectionView = {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout.init())
+        cv.register(NoteCollectionCell.self, forCellWithReuseIdentifier: NoteCollectionCell.reusableId)
+        cv.delegate = self
+        cv.dataSource = self
+        cv.isScrollEnabled = true
+        cv.bounces = true
+        cv.backgroundColor = .clear
+        cv.showsVerticalScrollIndicator = true
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+    func setupViews(){
+        view.addSubview(notesCollectionView)
+        configureCompositionalLayout()
+    }
+    func configureNavBar(){
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: Font.semi_bold.rawValue, size: 16.0)!,NSAttributedString.Key.foregroundColor: Color.dark]
+        self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.font: UIFont(name: Font.bold.rawValue, size: 28.0)!,NSAttributedString.Key.foregroundColor: Color.dark]
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationItem.largeTitleDisplayMode = .always
+      
+        
+        
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+    }
+    func setupContraints(){
+        NSLayoutConstraint.activate([
+            notesCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            notesCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 4),
+            notesCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -4),
+            notesCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    func configureCompositionalLayout(){
 
+        let groupOneRightItem = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/2)))
+        groupOneRightItem.contentInsets = .init(top: 8, leading: 6, bottom: 0, trailing: 6)
+        
+        let groupOneLeftItem = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1)))
+        groupOneLeftItem.contentInsets = .init(top: 8, leading: 6, bottom: 0, trailing: 6)
+        
+        let innerLeftGroupOne = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1)), subitems: [groupOneRightItem])
+        
+        let groupOne = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(400)), subitems: [innerLeftGroupOne,groupOneLeftItem])
+        
+        let groupTwo = NSCollectionLayoutItem.init(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)))
+        groupTwo.contentInsets = .init(top: 8, leading: 6, bottom: 0, trailing: 6)
+        
+        let groupThreeItem = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1)))
+        groupThreeItem.contentInsets = .init(top: 8, leading: 6, bottom: 0, trailing: 6)
+        
+        let groupThree = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/2)), subitems: [groupThreeItem])
+        
+        let containerGroup = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(600)), subitems: [groupOne,groupTwo,groupThree])
+        
+        let section = NSCollectionLayoutSection.init(group: containerGroup)
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.contentInsetsReference = .none
+    
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        notesCollectionView.setCollectionViewLayout(layout, animated: true)
+      
+    }
 }
+
+extension NotesVC: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return notesData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NoteCollectionCell.reusableId, for: indexPath) as! NoteCollectionCell
+        cell.data = notesData[indexPath.row]
+        cell.bodyLabel.setLineHeight(lineHeight: 1.4)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let item = notesData[indexPath.row]
+        print(item.heading)
+    }
+    
+    
+}
+
--- Notes-Clone/Models/MockData.swift
+++ Notes-Clone/Models/MockData.swift
@@ -33,7 +33,8 @@ struct CategoryType{
         CategoryType(title:"💍 Wedding"),
         CategoryType(title:"🎶 Music"),
         CategoryType(title:"💼 Work"),
-        CategoryType(title:"⚙️ Electronic"),
+        CategoryType(title:"🙇‍♂️ Personal"),
+        CategoryType(title:"⚙️ Electronics"),
         CategoryType(title:"🥳 Events"),
         CategoryType(title:"👍🏽 Likes"),
         CategoryType(title:"🌚 Disklikes"),
@@ -45,6 +46,27 @@ struct CategoryType{
         CategoryType(title:"👨‍💻 Technology"),
         CategoryType(title:"🔗 General Links"),
         CategoryType(title:"🏋🏽‍♀️ Workout"),
-        CategoryType(title:"🎉 Birthdays")
+        CategoryType(title:"🎉 Birthdays"),
+        CategoryType(title:"⚽️ Football"),
+        CategoryType(title:"💬 Popular Quotes"),
+        CategoryType(title:"🔬 Research"),
+    ]
+}
+
+
+struct NoteType{
+    var heading: String
+    var body: String
+    
+    static let data:[NoteType] = [
+        NoteType(heading: "Notice how Task 1, Task 2", body: "Task 3 start quickly one after the other. On the other hand, Task 1 took a while to start after Task 0. Also notice that while Task 3 started after Task 2, it finished first."),
+        NoteType(heading: "three main types of queues", body: "Main queue: runs on the main thread and is a serial queue."),
+        NoteType(heading: "Global queues:", body: "concurrent queues that are shared by the whole system. There are four such queues with different priorities : high, default, low, and background."),
+        NoteType(heading: "Custom queues:", body: "queues that you create which can be serial or concurrent. Requests in these queues actually end up in one of the global queues."),
+        NoteType(heading: "User-interactive", body: "This represents tasks that must complete immediately in order to provide a nice user experience."),
+        NoteType(heading: "User-initiated: The user initiates", body: "these asynchronous tasks from the UI. Use them when the user is waiting for immediate results and for tasks required to continue user interaction."),
+        NoteType(heading: "Utility", body: " This represents long-running tasks, typically with a user-visible progress indicator. Use it for computations"),
+        NoteType(heading: "Synchronous vs. Asynchronous", body: "With GCD, you can dispatch a task either synchronously or asynchronously. A synchronous function returns control to the caller after the task completes. You can schedule a unit of work synchronously by calling DispatchQueue.sync(execute:)."),
+        NoteType(heading: "Managing Tasks", body: "You’ve heard about tasks quite a bit by now. For the purposes of this tutorial you can consider a task to be a closure. Closures are self-contained, callable blocks of code you can store and pass around."),
     ]
 }
