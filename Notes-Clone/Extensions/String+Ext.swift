eca6c37f9d279b12843d708e06055576ddefd344
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-24 12:59:28 +0000

    user initials

---
+++ Notes-Clone/Extensions/String+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  String+Ext.swift
+//  Notes-Clone
+//
+//  Created by Kwame Agyenim - Boateng on 24/10/2022.
+//
+
+import Foundation
+
+extension String {
+    
+    func getUserInitials() -> String{
+
+        let names = self.components(separatedBy: " ")
+
+        let firstNameInitials = names[0].prefix(1)
+        let lastNameInitials = names[1].prefix(1)
+    
+        return  "\(firstNameInitials)\(lastNameInitials)".uppercased()
+    }
+}
