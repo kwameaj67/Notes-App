245e98f6f42ab0208d37c65df63b38de9e3f3b9f
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-28 13:27:29 +0000

    complete delete folder from coredata

--- Notes-Clone/Controllers/Folder/FolderVC.swift
+++ Notes-Clone/Controllers/Folder/FolderVC.swift
@@ -12,9 +12,13 @@ import BottomSheet
 class FolderVC: UIViewController, SaveFolderDelegate {
    
     private var cancellables: AnyCancellable?
+    let childVC = FolderOptionsVC()
+    var folderIndexPath: IndexPath = IndexPath(row: 0, section: 0)
     private var folders:[Folder] = [] {
         didSet{
-            folderTableView.reloadData()
+            DispatchQueue.main.async {
+                self.folderTableView.reloadData()
+            }
             if folders.count > 0 {
                 folderTableView.isHidden = false
                 folderTableView.alpha = 1
@@ -30,7 +34,6 @@ class FolderVC: UIViewController, SaveFolderDelegate {
         super.viewDidLoad()
         title = "folders"
         view.backgroundColor = Color.dark
-        configureNavBar()
         setupViews()
         setupContraints()
         getFolderData()
@@ -96,7 +99,7 @@ class FolderVC: UIViewController, SaveFolderDelegate {
         
         emptyLabel.attributedText = setupAttributedText("Wow, such empty 😬", "You have no folders created")
     }
-  
+    
     func setupContraints(){
         NSLayoutConstraint.activate([
             emptyLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
@@ -105,7 +108,7 @@ class FolderVC: UIViewController, SaveFolderDelegate {
             folderTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             folderTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 10),
             folderTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -10),
-            folderTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            folderTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             
             addButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
             addButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
@@ -125,21 +128,23 @@ class FolderVC: UIViewController, SaveFolderDelegate {
             getFolderData()
         }
     }
-    
-    func setupAttributedText(_ title: String,_ subTitle: String) -> NSAttributedString{
-        let text = NSMutableAttributedString(string: title, attributes: [.foregroundColor: UIColor.systemGray2,.font: UIFont(name: Font.semi_bold.rawValue, size: 18)!])
-        text.append(NSAttributedString(string: "\n\n\(subTitle)", attributes: [.foregroundColor: UIColor.systemGray2.withAlphaComponent(0.8),.font: UIFont(name: Font.medium.rawValue, size: 16)!]))
-        return text
-    }
   
-    @objc func didTapMoreImage(){
-        print("")
+    @objc func didTapMoreImage(_ sender: UIButton){
+        childVC.preferredContentSize  = CGSize(width: Int(view.frame.width), height: 180)
+        childVC.delegate = self
+        presentBottomSheet(
+            viewController: childVC,
+            configuration:  BottomSheetConfiguration(
+                cornerRadius: 30,
+                pullBarConfiguration: .visible(.init(height: 20)),
+                shadowConfiguration: .init(backgroundColor: UIColor.black.withAlphaComponent(0.6))
+            ))
     }
 }
 
 extension FolderVC:UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return folders.count
+        return 1
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -153,14 +158,15 @@ extension FolderVC:UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let item = folders[indexPath.section]
+        let folder = folders[indexPath.row]
         let vc = NotesVC()
-        vc.folderTitle = item.heading ?? "No title"
+        print(folder.heading!)
+        vc.folderTitle = folder.heading ?? "No title"
         navigationController?.pushViewController(vc, animated: true)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 205.0
+        return 215.0
     }
     
 }
@@ -168,14 +174,14 @@ extension FolderVC:UITableViewDelegate {
 extension FolderVC: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return folders.count
     }
    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: FolderCell.reusableId, for: indexPath) as! FolderCell
-        cell.setupCell(for: folders[indexPath.section])
+        cell.data = folders[indexPath.row]
+        cell.delegate = self
         cell.controller = self
-        cell.layer.cornerRadius = 20
         cell.selectionStyle = .none
         return cell
     }
@@ -203,4 +209,35 @@ extension FolderVC {
         navigationController?.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: .none, action: .none)
         navigationController?.navigationBar.tintColor = Color.dark
     }
+    
+    func setupAttributedText(_ title: String,_ subTitle: String) -> NSAttributedString{
+        let text = NSMutableAttributedString(string: title, attributes: [.foregroundColor: UIColor.systemGray2,.font: UIFont(name: Font.semi_bold.rawValue, size: 18)!])
+        text.append(NSAttributedString(string: "\n\n\(subTitle)", attributes: [.foregroundColor: UIColor.systemGray2.withAlphaComponent(0.8),.font: UIFont(name: Font.medium.rawValue, size: 16)!]))
+        return text
+    }
+}
+
+extension FolderVC: DeleteFolderDelegate, OptionCellDelegate {
+    func getCellPressed(in cell: UITableViewCell) {
+        guard let indexPath = folderTableView.indexPath(for: cell) else {
+               return
+           }
+        self.folderIndexPath = IndexPath(row: indexPath.row, section: 0)
+    }
+    
+    
+    func deleteFolderItem() {
+        // get folder object to delete
+        let item = folderIndexPath
+        let folderObject  = self.folders[item.row]
+        // delete object
+        viewModel.deleteFolder(folder: folderObject)
+        // remove folder from row & tableview
+        folders.remove(at:  item.row)
+        folderTableView.deleteRows(at: [folderIndexPath], with: .fade)
+        DispatchQueue.main.async {
+            self.folderTableView.reloadData()
+        }
+    }
+   
 }
