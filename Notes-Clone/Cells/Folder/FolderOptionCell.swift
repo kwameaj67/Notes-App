35fcefbca0c90143a3f5d2c89db284ad95e0e3ed
Kwame <kwameagyenimboateng64@gmail.com>
2022-10-28 13:26:25 +0000

    bottom sheet tableview cell

---
+++ Notes-Clone/Cells/Folder/FolderOptionCell.swift
@@ -0,0 +1,61 @@
+//
+//  FolderOptionCell.swift
+//  Notes-Clone
+//
+//  Created by Kwame Agyenim - Boateng on 27/10/2022.
+//
+
+import UIKit
+
+class FolderOptionCell: UITableViewCell {
+
+    static let reusableId = "FolderOptionCell"
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: FolderOptionCell.reusableId)
+        setupViews()
+        setupContraints()
+        backgroundColor = .clear
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // MARK: Properties -
+    let titleLabel: UILabel = {
+        let lb = UILabel()
+        lb.font = UIFont(name: Font.medium.rawValue, size: 18)
+        lb.textColor = .systemGray2
+        lb.numberOfLines = 0
+        lb.translatesAutoresizingMaskIntoConstraints = false
+        return lb
+    }()
+    let icon : UIImageView = {
+        var iv = UIImageView()
+        iv.tintColor = .systemGray2
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    func setupViews(){
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(icon)
+    }
+    func setupContraints(){
+        NSLayoutConstraint.activate([
+            
+            icon.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            icon.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30),
+            icon.heightAnchor.constraint(equalToConstant: 25),
+            icon.widthAnchor.constraint(equalToConstant: 25),
+            
+            titleLabel.centerYAnchor.constraint(equalTo: icon.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -30),
+        ])
+    }
+    func setupCell(item: FolderOptionType){
+        titleLabel.text = item.name
+        icon.image = UIImage(named: item.icon)?.withRenderingMode(.alwaysTemplate)
+    }
+}
