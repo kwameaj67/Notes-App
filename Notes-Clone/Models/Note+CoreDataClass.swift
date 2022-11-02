c1df823950d25acca6df2587da201351f9f1a6e3
Kwame <kwameagyenimboateng64@gmail.com>
2022-11-02 00:11:06 +0000

    date comparable on note

--- Notes-Clone/Models/Note+CoreDataClass.swift
+++ Notes-Clone/Models/Note+CoreDataClass.swift
@@ -10,6 +10,11 @@ import Foundation
 import CoreData
 
 @objc(Note)
-public class Note: NSManagedObject {
-
+public class Note: NSManagedObject, Comparable {
+    public static func < (lhs: Note, rhs: Note) -> Bool {
+        if lhs.createdAt! < rhs.createdAt! {
+            return true
+        }
+        return false
+    }
 }
