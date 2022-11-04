8c45a8ea5646ea1967944ea9ce1480cbcd1ba3b8
Kwame <kwameagyenimboateng64@gmail.com>
2022-11-04 19:19:53 +0000

    changed textField textColor

--- Notes-Clone/Views/NTextField.swift
+++ Notes-Clone/Views/NTextField.swift
@@ -13,7 +13,7 @@ class NTextField: UITextField {
         super.init(frame: frame)
         borderStyle = .none
         backgroundColor = Color.pad_bg
-        textColor = Color.text_color_normal
+        textColor = Color.dark
         font = UIFont(name: Font.medium.rawValue, size: 15)
         layer.masksToBounds = true
         layer.borderWidth = 0.0
